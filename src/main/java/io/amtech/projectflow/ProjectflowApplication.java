ca240e491ee02c76175f99081ae9ddcb88ca3dc3
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2021-05-13 11:28:26 +0300

    Initial struct

---
+++ src/main/java/io/amtech/projectflow/ProjectflowApplication.java
@@ -0,0 +1,11 @@
+package io.amtech.projectflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectflowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectflowApplication.class, args);
+    }
+}
