8365ddf373584ff548526e8458bd2dca36bda8d6
KiraLis39 <alexnestor2020@gmail.com>
2021-05-25 16:06:30 +0300

    #10 added successful testcases and failed testcases on UPDATE Entities

--- src/test/java/io/amtech/projectflow/rest/employee/EmployeeControllerTest.java
+++ src/test/java/io/amtech/projectflow/rest/employee/EmployeeControllerTest.java
@@ -11,9 +11,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlMergeMode;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static io.amtech.projectflow.domain.employee.UserPosition.DIRECTOR;
@@ -25,114 +27,112 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 class EmployeeControllerTest extends IntegrationTest {
 
-    private static final String CREATE_URL = "/employees";
-    private static final String UPDATE_URL = "/employees/1";
+    private static final String BASE_URL = "/employees";
     @Autowired
     EmployeeRepository repository;
 
+    static Stream<Arguments> createSuccessTestArgs() {
+        final String fakeName = strMultiple("a", 255);
+        final String fakeEmail = strMultiple("b", 38) + "@example.com";
+        final String fakePhone = strMultiple("1", 50);
+        return Stream.of(
+                Arguments.arguments(buildJson("createSuccessTest/full_request.json"),
+                        buildJson("createSuccessTest/full_response.json"),
+                        new Employee()
+                                .setId(1L)
+                                .setName("Иван Копыто")
+                                .setEmail("kopito@example.com")
+                                .setPhone("+7 128 123 12 12")
+                                .setPosition(PROJECT_LEAD)),
+                Arguments.arguments(buildJson("createSuccessTest/without_phone_request.json"),
+                        buildJson("createSuccessTest/without_phone_response.json"),
+                        new Employee()
+                                .setId(1L)
+                                .setName("А")
+                                .setEmail("a@b.ru")
+                                .setPosition(DIRECTOR)),
+                Arguments.arguments(buildJson("default_request.json.template", fakeName,
+                        fakeEmail,
+                        fakePhone,
+                        PROJECT_LEAD.name()),
+                        buildJson("createSuccessTest/default_response.json.template", 1, fakeName,
+                                fakeEmail,
+                                fakePhone,
+                                PROJECT_LEAD.name()),
+                        new Employee()
+                                .setId(1L)
+                                .setName(fakeName)
+                                .setEmail(fakeEmail)
+                                .setPhone(fakePhone)
+                                .setPosition(PROJECT_LEAD))
+        );
+    }
 
-    // success create:
-//    static Stream<Arguments> createSuccessTestArgs() {
-//        final String fakeName = strMultiple("a", 255);
-//        final String fakeEmail = strMultiple("b", 38) + "@example.com";
-//        final String fakePhone = strMultiple("1", 50);
-//        return Stream.of(
-//                Arguments.arguments(buildJson("createSuccessTest/full_request.json"),
-//                        buildJson("createSuccessTest/full_response.json"),
-//                        new Employee()
-//                                .setId(1L)
-//                                .setName("Иван Копыто")
-//                                .setEmail("kopito@example.com")
-//                                .setPhone("+7 128 123 12 12")
-//                                .setPosition(PROJECT_LEAD)),
-//                Arguments.arguments(buildJson("createSuccessTest/without_phone_request.json"),
-//                        buildJson("createSuccessTest/without_phone_response.json"),
-//                        new Employee()
-//                                .setId(1L)
-//                                .setName("А")
-//                                .setEmail("a@b.ru")
-//                                .setPosition(DIRECTOR)),
-//                Arguments.arguments(buildJson("default_request.json.template",
-//                        fakeName,
-//                        fakeEmail,
-//                        fakePhone,
-//                        PROJECT_LEAD.name()),
-//                        buildJson("createSuccessTest/default_response.json.template",
-//                                1L,
-//                                fakeName,
-//                                fakeEmail,
-//                                fakePhone,
-//                                PROJECT_LEAD.name()),
-//                        new Employee()
-//                                .setId(1L)
-//                                .setName(fakeName)
-//                                .setEmail(fakeEmail)
-//                                .setPhone(fakePhone)
-//                                .setPosition(PROJECT_LEAD))
-//        );
-//    }
+    @ParameterizedTest
+    @MethodSource("createSuccessTestArgs")
+    @SneakyThrows
+    @Sql(scripts = {
+            "classpath:db/EmployeeControllerTest/createSuccessTest/exists_employees.sql"
+    })
+    void createSuccessTest(final String request, final String response, final Employee e) {
+        // setup
 
-//    @ParameterizedTest
-//    @MethodSource("createSuccessTestArgs")
-//    @SneakyThrows
-//    @Sql(scripts = {
-//            "classpath:db/EmployeeControllerTest/createSuccessTest/createEmployees.sql"
-//    })
-//    void createSuccessTest(final String request, final String response, final Employee e) {
-//        // setup
-//
-//        // when
-//        mvc.perform(TestUtils
-//                .createPost(BASE_URL)
-//                .content(request))
-//                .andExpect(status().isOk())
-//                .andExpect(content().json(response, true));
-//
-//        // then
-//        Assertions.assertThat(txUtil.txRun(() -> repository.findAll()))
-//                .hasSize(3);
-//
-//        Assertions.assertThat(txUtil.txRun(() -> repository.findById(1L)))
-//                .isPresent()
-//                .contains(e);
-//    }
+        // when
+        mvc.perform(TestUtils
+                .createPost(BASE_URL)
+                .content(request))
+                .andExpect(status().isOk())
+                .andExpect(content().json(response, true));
 
+        // then
+        Assertions.assertThat(txUtil.txRun(() -> repository.findAll()))
+                .hasSize(3);
 
-    // failed create:
-//    static Stream<Arguments> createFailTestArgs() {
-//        final String fakeName = strMultiple("a", 256);
-//        return Stream.of(
-//                Arguments.arguments(buildJson("createFailTest/name_is_missing_request.json"),
-//                        buildJson("createFailTest/name_is_missing_response.json"),
-//                        400),
-//                Arguments.arguments(buildJson("default_request.json.template",
-//                        fakeName, "sd@mail.com", "293 3993 93939", PROJECT_LEAD),
-//                        buildJson("createFailTest/name_is_too_long_response.json"),
-//                        400)
-//        );
-//    }
+        Assertions.assertThat(txUtil.txRun(() -> repository.findById(1L)))
+                .isPresent()
+                .contains(e);
+    }
 
-//    @ParameterizedTest
-//    @MethodSource("createFailTestArgs")
-//    @SneakyThrows
-//    void createFailTest(final String request, final String response, int httpStatus) {
-//        // setup
-//        mvc.perform(TestUtils
-//                .createPost(BASE_URL)
-//                .content(request))
-//                .andExpect(status().is(httpStatus))
-//                .andExpect(content().json(response, true));
-//
-//        // then
-//        Assertions.assertThat(txUtil.txRun(() -> repository.findAll()))
-//                .isEmpty();
-//    }
+    static Stream<Arguments> createFailTestArgs() {
+        final String fakeName = strMultiple("a", 256);
+        return Stream.of(
+                Arguments.arguments(buildJson("createFailTest/name_is_missing_request.json"),
+                        buildJson("createFailTest/name_is_missing_response.json"),
+                        400),
+                Arguments.arguments(buildJson("default_request.json.template",
+                        fakeName, "sd@mail.com", "293 3993 93939", PROJECT_LEAD),
+                        buildJson("createFailTest/name_is_too_long_response.json"),
+                        400)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("createFailTestArgs")
+    @SneakyThrows
+    void createFailTest(final String request, final String response, int httpStatus) {
+        // setup
+        mvc.perform(TestUtils
+                .createPost(BASE_URL)
+                .content(request))
+                .andExpect(status().is(httpStatus))
+                .andExpect(content().json(response, true));
+
+        // then
+        Assertions.assertThat(txUtil.txRun(() -> repository.findAll()))
+                .isEmpty();
+    }
 
     // success update:
     static Stream<Arguments> updateSuccessArgs() {
         return Stream.of(
-                Arguments.arguments(
-                        buildJson("updateSuccessTest/updateRequestTest.json")
+                Arguments.arguments(1,
+                        buildJson("updateSuccessTest/full_update_request.json")
+                ),
+                Arguments.arguments(1,
+                        buildJson("updateSuccessTest/fired_update_request.json")
+                ),
+                Arguments.arguments(1,
+                        buildJson("updateSuccessTest/phone_position_update_request.json")
                 )
         );
     }
@@ -141,47 +141,40 @@ class EmployeeControllerTest extends IntegrationTest {
     @MethodSource("updateSuccessArgs")
     @SneakyThrows
     @Sql(scripts = {
-            "classpath:db/clean.sql",
             "classpath:db/EmployeeControllerTest/updateSuccessTest/createEmployees.sql"
     })
-    void updateSuccessTest(final String request) {
+    void updateSuccessTest(long id, final String request) {
         mvc.perform(TestUtils
-                .createPut(UPDATE_URL)
+                .createPut(BASE_URL + "/" + id)
                 .content(request))
                 .andExpect(status().isOk());
 
-//        Assertions.assertThat(txUtil.txRun(() -> repository.findAll()))
-//                .hasSize(1);
-
         Assertions.assertThat(txUtil.txRun(() -> repository.findById(1L)))
                 .isPresent();
+        Assertions.assertThat(txUtil.txRun(() -> repository.findAll()))
+                .hasSize(1);
     }
 
-    // failed update:
-//    static Stream<Arguments> createFailedUpdateArgs() {
-//        return Stream.of(
-//                Arguments.arguments(
-//
-//                )
-//        );
-//    }
-//    @ParameterizedTest
-//    @MethodSource("createSuccessTestArgs")
-//    @SneakyThrows
-//    @Sql(scripts = {
-//            "classpath:db/EmployeeControllerTest/createSuccessTest/createEmployees.sql"})
-//    void updateFailTest(final String request, final String response, int httpStatus) throws Exception {
-//        // setup
-//
-//        mvc.perform(TestUtils
-//                .createPut(BASE_URL + "/${id}" ).
-//                .content(request));
-//
-//        // then
-//        Assertions.assertThat(txUtil.txRun(() -> repository.findAll()))
-//                .isEmpty();
-//    }
+    static Stream<Arguments> updateFailedArgs() {
+        return Stream.of(
+                Arguments.arguments(1, buildJson("updateFailTest/update_request_fired_is_null.json"), HttpStatus.BAD_REQUEST.value()),
+                Arguments.arguments(2, buildJson("updateFailTest/update_request_empty_failed.json"), HttpStatus.BAD_REQUEST.value()),
+                Arguments.arguments(12, buildJson("updateFailTest/unexisting_id_call_failed.json"), HttpStatus.NOT_FOUND.value())
+        );
+    }
 
+    @ParameterizedTest
+    @MethodSource("updateFailedArgs")
+    @SneakyThrows
+    @Sql(scripts = {
+            "classpath:db/EmployeeControllerTest/updateSuccessTest/createEmployees.sql"
+    })
+    void updateFailTest(long id, final String request, int httpStatus) {
+        mvc.perform(TestUtils
+                .createPut(BASE_URL + "/" + id)
+                .content(request))
+                .andExpect(status().is(httpStatus));
+    }
 
     private static String buildJson(final String resource, Object...args) {
         String template = TestUtils.readClassPathResourceAsString(
