6a50d34890663a59e2f722987d68575a2ab6ad62
KiraLis39 <angelicalis39@mail.ru>
2021-06-02 15:00:42 +0300

    #10 update+clean+refactoring

--- src/main/java/io/amtech/projectflow/domain/Notification.java
+++ src/main/java/io/amtech/projectflow/domain/Notification.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
+import java.sql.Date;
 
 @Data
 @Accessors(chain = true)
@@ -15,8 +16,11 @@ public class Notification {
     @Column(name = "id")
     private Long id;
 
-//    recepient varchar(50) not null check (recepient ~ $$^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$$),
-//    sender varchar(50) not null check (sender ~ $$^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$$),
+    @Column(name = "recepient", nullable = false)
+    String recepient;   //    recepient varchar(50) not null check (recepient ~ $$^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$$),
+
+    @Column(name = "sender", nullable = false)
+    String sender;  //    sender varchar(50) not null check (sender ~ $$^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$$),
 
     @Column(name = "subject")
     String subject;
@@ -25,5 +29,5 @@ public class Notification {
     String body;
 
     @Column(name = "create_date", nullable = false)
-    Object createDate;
+    Date createDate;
 }
--- src/main/java/io/amtech/projectflow/domain/project/Project.java
+++ src/main/java/io/amtech/projectflow/domain/project/Project.java
@@ -1,12 +1,15 @@
 package io.amtech.projectflow.domain.project;
 
 import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
+import io.amtech.projectflow.domain.Direction;
+import io.amtech.projectflow.domain.employee.Employee;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
+import java.sql.Date;
 
 @Data
 @Accessors(chain = true)
@@ -25,31 +28,22 @@ public class Project {
     @Column(name = "name", nullable = false)
     String name;
 
-    @Column(name = "project_lead_id")
-    long projectLeadId;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "project_lead")
+    Employee projectLead;
 
-//    @ManyToOne(fetch = FetchType.EAGER)
-//    @JoinColumn(name = "project_lead")
-//    Employee projectLead;
-
-    @Column(name = "direction_id")
-    long directionId;
-
-//    @OneToOne(cascade = CascadeType.ALL)
-//    @JoinColumn(name = "direction", referencedColumnName = "id")
-//    Direction direction;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "direction", referencedColumnName = "id")
+    Direction direction;
 
     @Column(name = "description")
     String description;
 
     @Column(name = "create_date", nullable = false)
-    Object createDate;
+    Date createDate;
 
     @Enumerated(EnumType.STRING)
-    @Column(columnDefinition = "project", nullable = false)
+    @Column(columnDefinition = "status", nullable = false)
     @Type( type = "project_status")
     private ProjectStatus projectStatus;
-
-//    constraint project_has_project_lead_fk foreign key(project_lead_id) references pf.employee(id) on delete restrict on update cascade,
-//    constraint project_has_direction_fk foreign key(direction_id) references pf.direction(id) on delete restrict on update cascade
 }
