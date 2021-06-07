fc814b907bb9f27cfac0a2c0c2fc04d33a356514
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2021-06-07 09:27:01 +0300

    #20 Remove ProjectRepository.java and uncomment test.

--- src/main/java/io/amtech/projectflow/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package io.amtech.projectflow.repository;
-
-import io.amtech.projectflow.domain.project.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository  extends JpaRepository<Project, Long> {
-}
