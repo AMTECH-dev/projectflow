3699ea28fa9434c960f7224d967f269a12c2cdfa
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2021-06-10 20:25:38 +0300

    #25 Added rest controller test.

---
+++ src/test/java/io/amtech/projectflow/rest/project_journal/ProjectJournalControllerTest.java
@@ -0,0 +1,150 @@
+package io.amtech.projectflow.rest.project_journal;
+
+import io.amtech.projectflow.repository.ProjectJournalRepository;
+import io.amtech.projectflow.test.IntegrationTest;
+import io.amtech.projectflow.test.TestUtils;
+import lombok.SneakyThrows;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.stream.Stream;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class ProjectJournalControllerTest extends IntegrationTest {
+    private static final String BASE_URL = "/projects/{projectId}/projectJournals";
+    private static final String BASE_ID_URL = BASE_URL + "/%d";
+    @Autowired
+    private ProjectJournalRepository projectJournalRepository;
+
+    @SuppressWarnings("unused")
+    static Stream<Arguments> searchSuccessArgs() {
+        return Stream.of(
+                Arguments.arguments(2, "?", buildJson("searchSuccessTest/all.json"), HttpStatus.OK.value()),
+                Arguments.arguments(3, "?login=I&orders=-id",
+                        buildJson("searchSuccessTest/contains_i_with_order_id_desc.json"), HttpStatus.OK.value()),
+                Arguments.arguments(2, "?login=anna", buildJson("searchSuccessTest/contains_anna.json"),
+                        HttpStatus.OK.value()),
+                Arguments.arguments(2, "?login=a&updateDateFrom=1173440943&updateDateTo=1607514543&orders=login",
+                        buildJson("searchSuccessTest/contains_a_between_date_with_order_login.json"),
+                        HttpStatus.OK.value()),
+                Arguments.arguments(1, "?",
+                        buildJson("searchSuccessTest/empty_journal_in_project.json"),
+                        HttpStatus.OK.value())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("searchSuccessArgs")
+    @SneakyThrows
+    @Sql(scripts = {
+            "classpath:db/ProjectJournalControllerTest/searchSuccessTest/data.sql"
+    })
+    void searchSuccessTest(long projectId, final String url, final String response, int status) {
+        String finalUrlAddress = replaceProjectIdInURLOn(BASE_URL, projectId) + url;
+        mvc.perform(TestUtils
+                .createGet(finalUrlAddress))
+                .andDo(print())
+                .andExpect(status().is(status))
+                .andExpect(content().json(response, true));
+    }
+
+    @SuppressWarnings("unused")
+    static Stream<Arguments> searchFailArgs() {
+        return Stream.of(
+                Arguments.arguments(-1, "", buildJson("searchFailTest/project_not_found.json"),
+                        HttpStatus.NOT_FOUND.value()),
+                Arguments.arguments(1, "?orders=fieldname", buildJson("searchFailTest/invalid_order_response.json"),
+                        HttpStatus.BAD_REQUEST.value())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("searchFailArgs")
+    @SneakyThrows
+    @Sql(scripts = {
+            "classpath:db/ProjectJournalControllerTest/searchFailTest/data.sql"
+    })
+    void searchFailTest(long projectId, final String url, final String response, int status) {
+        String finalUrlAddress = replaceProjectIdInURLOn(BASE_URL, projectId) + url;
+        mvc.perform(TestUtils
+                .createGet(finalUrlAddress))
+                .andDo(print())
+                .andExpect(content().json(response, true))
+                .andExpect(status().is(status));
+    }
+
+    @SuppressWarnings("unused")
+    static Stream<Arguments> getSuccessArgs() {
+        return Stream.of(
+                Arguments.arguments(2, 3, buildJson("getSuccessTest/second_project_3th_journal_response.json"),
+                        HttpStatus.OK.value()),
+                Arguments.arguments(1, 5, buildJson("getSuccessTest/first_project_5th_journal_response.json"),
+                        HttpStatus.OK.value())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getSuccessArgs")
+    @SneakyThrows
+    @Sql(scripts = {
+            "classpath:db/ProjectJournalControllerTest/getSuccessTest/data.sql"
+    })
+    void getSuccessTest(long projectId, long projectJournalId, final String response, int status) {
+        Assertions.assertThat(projectJournalRepository.findById(projectJournalId))
+                .isPresent();
+
+        String finishUrlAddress = String.format(replaceProjectIdInURLOn(BASE_ID_URL, projectId), projectJournalId);
+        mvc.perform(TestUtils
+                .createGet(finishUrlAddress))
+                .andDo(print())
+                .andExpect(content().json(response, true))
+                .andExpect(status().is(status));
+    }
+
+    @SuppressWarnings("unused")
+    static Stream<Arguments> getFailArgs() {
+        return Stream.of(
+                Arguments.arguments(-100, 28147, buildJson("getFailTest/project_not_found.json"),
+                        HttpStatus.NOT_FOUND.value()),
+                Arguments.arguments(1, 28147, buildJson("getFailTest/project_journal_not_found_response.json"),
+                        HttpStatus.NOT_FOUND.value())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getFailArgs")
+    @SneakyThrows
+    @Sql(scripts = {
+            "classpath:db/ProjectJournalControllerTest/getFailTest/data.sql"
+    })
+    void getFailTest(long projectId, long projectJournalId, final String response, int status) {
+        Assertions.assertThat(projectJournalRepository.findById(projectJournalId))
+                .isNotPresent();
+
+        String finishUrlAddress = String.format(replaceProjectIdInURLOn(BASE_ID_URL, projectId), projectJournalId);
+        mvc.perform(TestUtils
+                .createGet(finishUrlAddress))
+                .andDo(print())
+                .andExpect(content().json(response, true))
+                .andExpect(status().is(status));
+    }
+
+    private String replaceProjectIdInURLOn(final String url, long projectId) {
+        return url.replace("{projectId}", String.valueOf(projectId));
+    }
+
+    private static String buildJson(final String resource, Object... args) {
+        String template = TestUtils.readClassPathResourceAsString(
+                "json/ProjectJournalControllerTest/" + resource);
+
+        return String.format(template, args);
+    }
+}
---
+++ src/test/resources/db/ProjectJournalControllerTest/getFailTest/data.sql
@@ -0,0 +1,25 @@
+insert into pf.employee(id, name, email, phone, position, is_fired)
+values (1, 'ОЛЕГ', 'my-email@gmail.com', '999999999', 'DIRECTOR', false),
+       (2, 'ВАНА', 'my-email@gmail.com', '999999999', 'DIRECTION_LEAD', false),
+       (4, 'ФЕДОР', 'my-email@gmail.com', '999999999', 'PROJECT_LEAD', false),
+       (10, 'Катя', 'my-email@gmail.com', '999999999', 'DIRECTION_LEAD', false),
+       (5, 'Йосип', 'my-email@gmail.com', '999999999', 'PROJECT_LEAD', false);
+
+insert into pf.direction(id, lead_id, name)
+values (1, 2, 'dev'),
+       (10, 10, 'test');
+
+insert into pf.project(name, project_lead_id, direction_id, description, create_date, status)
+values ('Hack NASA using HTML', 4, 10, '', current_timestamp, 'UNAPPROVED'),
+       ('DROP DATABASE ', 4, 1, '', '2022-07-02 15:31:59+003', 'ON_PL_PLANNING'),
+       ('Projectflow ', 4, 1, '', current_timestamp, 'ON_DL_APPROVING');
+
+insert into pf.project_journal(project_id, login, update_date, current_state)
+values (2, 'oleg', '2002-02-09T11:49:03', '{"a": "dwq", "v": "updated", "field": {"a": "dwq", "v": "updated", "field": "updated"}}'),
+       (3, 'sim', '2013-02-02T12:29:03', '{"a": "dwq", "v": "updated", "field": "updated"}'),
+       (2, 'vika', '2017-02-09T11:49:03', '{"a": "dwq", "v3": "some update", "field": "updated"}'),
+       (3, 'zina', '2015-02-09T11:49:03', '{"a": "UPDATe", "v2": "WOW", "field": "baby"}'),
+       (1, 'anna', '2020-03-09T11:49:03', '{"a": "dwq", "v1": "updated", "field": "create project"}'),
+       (2, 'olina', '2007-03-09T11:49:03', '{"a": "dwq", "some": {"message": "super update"}, "field": "wow nwe update"}'),
+       (2, 'anna', '2021-02-28T02:50:02', '{"a": "dwq", "v1": "updated", "field": "create project"}');
+
