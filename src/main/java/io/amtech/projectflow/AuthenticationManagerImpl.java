40baf74015639f9061bb947da5237fc7670f272d
KiraLis39 <angelicalis39@mail.ru>
2021-06-23 14:35:52 +0300

    #39 Мелкий рефакторинг

--- src/main/java/io/amtech/projectflow/AuthenticationManagerImpl.java
+++ src/main/java/io/amtech/projectflow/AuthenticationManagerImpl.java
@@ -1,39 +1,20 @@
 package io.amtech.projectflow;
 
-import io.amtech.projectflow.repository.AuthUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
 
 public class AuthenticationManagerImpl implements AuthenticationManager {
-    @Autowired
-    private AuthUserRepository authUserRepository;
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    static final List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
-    static {
-//        AUTHORITIES.add("ALL_ROLE_USER");
-    }
-
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-//        authentication = new TokenFilter(authentication);
-
         if (authentication.getName().equals(authentication.getCredentials())) {
             return new UsernamePasswordAuthenticationToken(authentication.getName(),
-                    authentication.getCredentials(), AUTHORITIES);
+                    authentication.getCredentials(), Collections.emptyList());
         }
         throw new BadCredentialsException("Bad Credentials");
-
-//        return authentication;
     }
 }
