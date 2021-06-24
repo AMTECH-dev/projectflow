32f44d46bf67aff17640ea9a8f3b29defcd0af19
VladislavBakshanskij <vbakshanskij2000@gmail.com>
2021-06-24 13:26:07 +0300

    #33 ...

--- src/main/java/io/amtech/projectflow/service/token/TokenService.java
+++ src/main/java/io/amtech/projectflow/service/token/TokenService.java
@@ -1,7 +1,5 @@
 package io.amtech.projectflow.service.token;
 
-import javax.servlet.http.HttpServletRequest;
-
 public interface TokenService {
     TokenPayload decode(String token);
     TokenDto generate(AuthDto dto);
