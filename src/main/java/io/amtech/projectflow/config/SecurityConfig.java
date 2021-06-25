1ccf22db68a15533e37584035de41078c32590f5
Igor Kocherzhenko <k.iv86@yandex.ru>
2021-06-25 13:48:40 +0300

    обновленно

--- src/main/java/io/amtech/projectflow/config/SecurityConfig.java
+++ src/main/java/io/amtech/projectflow/config/SecurityConfig.java
@@ -1,30 +1,63 @@
 package io.amtech.projectflow.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import io.amtech.projectflow.filter.TokenFilter;
+import io.amtech.projectflow.repository.AuthUserRepository;
+import io.amtech.projectflow.service.auth.AuthService;
+import io.amtech.projectflow.service.auth.impl.CustomAuthenticationManager;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AuthService authService;
+    private final AuthUserRepository authUserRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        TokenFilter filter = tokenFilter();
+
         http
-                .httpBasic().disable()
+                .cors()
+                .and()
                 .csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .addFilterAfter(filter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(HttpMethod.POST, "/auth/**")
+                .antMatchers("/swagger-ui", "/v2/api-docs");
     }
 
     @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder(12);
+    @Override
+    @SneakyThrows
+    public AuthenticationManager authenticationManagerBean() {
+        return new CustomAuthenticationManager(authUserRepository, authService);
+    }
+
+    @SneakyThrows
+    public TokenFilter tokenFilter() {
+        return new TokenFilter("/**", authService, authenticationManagerBean());
     }
 }
